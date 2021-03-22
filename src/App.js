848c7a6ec1f4eda3329a6d10c46654ab9d36cdeb
WebDices <webdices@gmail.com>
2021-03-22 14:28:27 +0200

    fetch data from edaman API & create recipe component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const APP_ID = "";
+  const APP_KEY = "";
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      ``
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Tessstt.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      {recipes.map((recipe) => (
+        <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
